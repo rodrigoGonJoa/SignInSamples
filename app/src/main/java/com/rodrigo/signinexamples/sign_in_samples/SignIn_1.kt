0c2abbf7fe50805e62ab723908816b526fe30e7c
Rodrigo González <rodrigogonzalezjoaquin@gmail.com>
2024-02-09 04:25:56 +0100

    The colors have been modified to be more in line with night mode

--- app/src/main/java/com/rodrigo/signinexamples/sign_in_samples/SignIn_1.kt
+++ app/src/main/java/com/rodrigo/signinexamples/sign_in_samples/SignIn_1.kt
@@ -1,7 +1,10 @@
 package com.rodrigo.signinexamples.sign_in_samples
 
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,23 +16,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
@@ -40,94 +49,124 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rodrigo.signinexamples.R
+import com.rodrigo.signinexamples.utils.rememberImeState
 
 @Composable
 fun SignIn1() {
-    val appName = "SoundBox"
+    val appName = "AppName"
     val highlightColor = Color(red = 224, green = 96, blue = 63, alpha = 255)
     val buttonHeight = 50.dp
     val shapeSizeValue = 30.dp
-    Column(
-        modifier = Modifier
-            .padding(32.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.weight(0.5f))
-        HeadLineText(appName)
-        Spacer(modifier = Modifier.size(16.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.Start)
-        ) {
-            BasicText(textValue = "New to $appName? ")
-            ClickableTextSample(
-                textValue = "Sign Up",
-                colorValue = highlightColor,
-                onTextClick = {/*TODO*/ })
-            BasicText(textValue = " now")
+    val imeState = rememberImeState()
+    val scrollState = rememberScrollState()
+
+    LaunchedEffect(key1 = imeState.value) {
+        if (imeState.value) {
+            scrollState.animateScrollTo(scrollState.maxValue, tween(300))
         }
-        Spacer(modifier = Modifier.size(16.dp))
-        OutsideSignInButton(
-            textValue = "Apple",
-            iconPath = R.drawable.apple_icon,
-            onButtonClicked = {/*TODO*/ },
-            backgroundColor = Color.White,
-            buttonHeight = buttonHeight,
-            shapeSize = shapeSizeValue
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(), painter = painterResource(R.drawable.background),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            alpha = 1f
         )
-        Spacer(modifier = Modifier.size(16.dp))
-        OutsideSignInButton(
-            textValue = "Google",
-            iconPath = R.drawable.google_icon,
-            onButtonClicked = {/*TODO*/ },
-            backgroundColor = Color.Black,
-            buttonHeight = buttonHeight,
-            shapeSize = shapeSizeValue
-        )
-        Spacer(modifier = Modifier.size(16.dp))
-        SignDivider()
-        Spacer(modifier = Modifier.size(8.dp))
-        InputDataComposable(
-            textValue = "Email",
-            placeholder = "Enter your email",
-            KeyboardType.Email
-        )
-        Spacer(modifier = Modifier.size(8.dp))
-        InputDataComposable(
-            textValue = "Password",
-            placeholder = "Enter your password",
-            KeyboardType.Password
-        )
-        Spacer(modifier = Modifier.size(16.dp))
         Box(
             modifier = Modifier
-                .wrapContentHeight()
-                .fillMaxWidth(),
-            contentAlignment = Alignment.CenterEnd
-        ) {
-            ClickableTextSample(
-                textValue = "Forgot Password?",
-                colorValue = highlightColor,
-                onTextClick = {/*TODO*/ })
-        }
-        Spacer(modifier = Modifier.size(16.dp))
-        Button(
+                .fillMaxSize()
+                .alpha(0.7f)
+                .background(
+                    if (isSystemInDarkTheme()) Color.Black else Color.White
+                )
+        )
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(buttonHeight),
-            onClick = { /*TODO*/ },
-            colors = ButtonDefaults.buttonColors(containerColor = highlightColor)
-        ) { Text(text = "Sign in") }
-        Spacer(modifier = Modifier.weight(1f))
-        RRSSButtons(shapeSizeValue)
+                .padding(32.dp)
+                .fillMaxSize()
+                .verticalScroll(state = scrollState),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(0.5f))
+            HeadLineText(appName)
+            Spacer(modifier = Modifier.size(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                BasicText(textValue = "New to $appName? ")
+                ClickableTextSample(
+                    textValue = "Sign Up",
+                    colorValue = highlightColor,
+                    onTextClick = {/*TODO*/ })
+                BasicText(textValue = " now")
+            }
+            Spacer(modifier = Modifier.size(16.dp))
+            OutsideSignInButton(
+                textValue = "Apple",
+                iconPath = R.drawable.apple_icon,
+                onButtonClicked = {/*TODO*/ },
+                backgroundColor = Color.White,
+                buttonHeight = buttonHeight,
+                shapeSize = shapeSizeValue
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            OutsideSignInButton(
+                textValue = "Google",
+                iconPath = R.drawable.google_icon,
+                onButtonClicked = {/*TODO*/ },
+                backgroundColor = Color.Black,
+                buttonHeight = buttonHeight,
+                shapeSize = shapeSizeValue
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            SignDivider()
+            Spacer(modifier = Modifier.size(8.dp))
+            InputDataComposable(
+                textValue = "Email",
+                placeholder = "Enter your email",
+                KeyboardType.Email
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            InputDataComposable(
+                textValue = "Password",
+                placeholder = "Enter your password",
+                KeyboardType.Password
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Box(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                ClickableTextSample(
+                    textValue = "Forgot Password?",
+                    colorValue = highlightColor,
+                    onTextClick = {/*TODO*/ })
+            }
+            Spacer(modifier = Modifier.size(16.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(buttonHeight),
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = highlightColor)
+            ) { Text(text = "Sign in",color = MaterialTheme.colorScheme.onSurface) }
+            Spacer(modifier = Modifier.weight(1f))
+            if (!imeState.value) {
+                RRSSButtons(shapeSizeValue)
+            }
+        }
     }
+
 }
 
 @Composable
-fun RRSSButtons(shapeSize:Dp){
+fun RRSSButtons(shapeSize: Dp) {
     Row {
         RoundBottom(
             size = 50,
@@ -179,7 +218,10 @@ fun RoundBottom(size: Int, imageIconPath: Int, onButtonClick: () -> Unit, shapeS
                 ),
             shape = RoundedCornerShape(size.dp),
             onClick = onButtonClick,
-            colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+            colors = ButtonDefaults.buttonColors(
+                containerColor =
+                if (isSystemInDarkTheme()) Color.Black else Color.White
+            )
         ) {}
         Image(
             modifier = Modifier.size(size.dp / 2),
@@ -195,7 +237,8 @@ fun InputDataComposable(textValue: String, placeholder: String, keyboardType: Ke
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
             text = textValue,
-            fontWeight = FontWeight.Medium
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onSurface
         )
         Spacer(modifier = Modifier.size(8.dp))
         OutlinedTextField(
@@ -217,11 +260,11 @@ fun SignDivider() {
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Divider(modifier = Modifier.weight(1f), thickness = 1.dp)
+        HorizontalDivider(modifier = Modifier.weight(1f), thickness = 1.dp, color = MaterialTheme.colorScheme.onSurface)
         Spacer(modifier = Modifier.size(8.dp))
-        Text(text = "Or")
+        Text(text = "Or",color = MaterialTheme.colorScheme.onSurface)
         Spacer(modifier = Modifier.size(8.dp))
-        Divider(modifier = Modifier.weight(1f), thickness = 1.dp)
+        HorizontalDivider(modifier = Modifier.weight(1f), thickness = 1.dp, color = MaterialTheme.colorScheme.onSurface)
     }
 }
 
@@ -251,7 +294,11 @@ fun OutsideSignInButton(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(modifier = Modifier.scale(0.9f), painter = painterResource(iconPath), contentDescription = "")
+            Image(
+                modifier = Modifier.scale(0.9f),
+                painter = painterResource(iconPath),
+                contentDescription = "",
+                )
             Spacer(Modifier.size(8.dp))
             Text(
                 text = "Sign in with $textValue",
@@ -267,13 +314,15 @@ fun HeadLineText(appName: String) {
         modifier = Modifier.fillMaxWidth(),
         text = "Sign to $appName",
         fontWeight = FontWeight.W900,
-        fontSize = 30.sp
+        fontSize = 30.sp,
+        color = MaterialTheme.colorScheme.onSurface
     )
 }
 
 @Composable
 fun BasicText(textValue: String) {
-    Text(text = textValue)
+    Text(text = textValue,
+        color = MaterialTheme.colorScheme.onSurface)
 }
 
 @Composable
